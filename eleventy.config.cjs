eb1773b6eaf5c600a76e596b83fdcb28a4ac17d2
Eric McCarthy <eric@limulus.net>
2023-10-29 21:11:11 -0700

    chore: set up eleventy

---
+++ eleventy.config.cjs
@@ -0,0 +1,13 @@
+const pluginWebc = require('@11ty/eleventy-plugin-webc')
+
+/** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
+module.exports = function (eleventyConfig) {
+  const input = 'www'
+  const output = 'dist/www'
+  const layouts = '_includes/layouts'
+  const components = `${input}/_include/components`
+
+  eleventyConfig.addPlugin(pluginWebc, { components })
+
+  return { dir: { input, output, layouts } }
+}
---
+++ www/index.md
@@ -0,0 +1,10 @@
+---
+title: A Web-Centric Ray Tracer Challenge Implementation
+layout: penumbra-page
+---
+
+# Penumbra
+
+_Penumbra_ is a [Ray Tracer
+Challenge](https://pragprog.com/titles/jbtracer/the-ray-tracer-challenge/) implementation
+built for the web.
