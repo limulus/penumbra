25c15564a30dccf74f27619b2323d97fb0827150
Eric McCarthy <eric@limulus.net>
2023-12-09 09:49:43 -0700

    chore(www): fix node_modules making it into dist/www

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -15,7 +15,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addGlobalData('site', { url: 'https://limulus.net/penumbra' })
 
   eleventyConfig.addPassthroughCopy(`${input}/assets`)
-  eleventyConfig.addPassthroughCopy('**/*.{png,svg,jpg,jpeg}')
+  eleventyConfig.addPassthroughCopy(`${input}/**/*.{png,svg,jpg,jpeg}`)
 
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin)
   eleventyConfig.addPlugin(pluginRss)
