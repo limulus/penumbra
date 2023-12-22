1fee85dad2f656c6d028a74d957b2129c0476f34
Eric McCarthy <eric@limulus.net>
2023-12-22 15:06:39 -0700

    chore: fix spelling

--- bin/check-for-tk.sh
+++ bin/check-for-tk.sh
@@ -5,7 +5,7 @@ STAGED_FILES=$(git diff --name-only --cached)
 for FILE in $STAGED_FILES
 do
     if grep -q "T""K" "$FILE"; then
-        echo "ERROR: You have a T""K in $FILE, please remove it before commiting"
+        echo "ERROR: You have a T""K in $FILE, please remove it before committing"
         exit 1
     fi
 done
