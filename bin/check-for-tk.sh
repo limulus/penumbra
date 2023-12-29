e29492cea45970df93a634471d7918b7147b332d
Eric McCarthy <eric@limulus.net>
2023-12-29 14:08:30 -0700

    chore: ignore binary files when checking for TK

--- bin/check-for-tk.sh
+++ bin/check-for-tk.sh
@@ -4,7 +4,7 @@ STAGED_FILES=$(git diff --name-only --cached)
 
 for FILE in $STAGED_FILES
 do
-    if grep -q "T""K" "$FILE"; then
+    if grep -Iq "T""K" "$FILE"; then
         echo "ERROR: You have a T""K in $FILE, please remove it before committing"
         exit 1
     fi
