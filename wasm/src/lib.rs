49e535027763b6f6075f827e02cc93c51a38e3f4
Eric McCarthy <eric@limulus.net>
2024-05-15 21:29:14 -0700

    chore: remove spoor functions

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,5 +1,4 @@
 use wasm_bindgen::prelude::*;
-use web_sys::ImageData;
 
 pub mod fuzzy;
 pub mod tuple;
@@ -24,30 +23,3 @@ pub fn run() -> Result<(), JsValue> {
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello from wasm-land!");
-}
-
-#[wasm_bindgen]
-pub fn pretty_gradient(width: usize, height: usize) -> ImageData {
-    let mut c = canvas::Canvas::new(width, height);
-
-    let magic = tuple::Tuple::new(c.width as f32 * 1.3333, c.height as f32 * 1.3333, 1.0, 1.0);
-    for y in 0..c.height {
-        for x in 0..c.width {
-            c.write_pixel(x, y, tuple::Tuple::new(x as f32, y as f32, 0.6666, 1.0) / magic);
-        }
-    }
-
-    c.to_image_data()
-}
