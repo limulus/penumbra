0823fbb209e6e6f0d7675099e3d8c5222057ef76
Eric McCarthy <609858+limulus@users.noreply.github.com>
2025-11-16 10:19:20 -0700

    chore: update wide dependency to latest version

--- wasm/src/fuzzy.rs
+++ wasm/src/fuzzy.rs
@@ -9,6 +9,5 @@ pub fn fuzzy_eq_f32(a: f32, b: f32) -> bool {
 pub fn fuzzy_eq_f32x4(a: f32x4, b: f32x4) -> bool {
     let diff = (a - b).abs();
     let epsilon = f32x4::splat(EPSILON);
-    let ge_epsilon = diff.cmp_ge(epsilon);
-    !ge_epsilon.any()
+    !diff.simd_ge(epsilon).any()
 }
